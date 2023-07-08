73e070cf019628e0460d12822ed9378531591b9f
Logan <pix.59700@gmail.com>
2023-07-08 02:54:56 +0200

    Add raycast player to interact object ✅

--- Assets/_Core/Scripts/Player/PlayerController.cs
+++ Assets/_Core/Scripts/Player/PlayerController.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Player player;
+    private Camera camera;
+    
     public float CameraClamp;
     public float Smooth;
+    public float CameraAngle;
     
     private float rotX;
     private float rotY;
     private float sens = 5f;
 
+    private void Start()
+    {
+        player = GetComponent<Player>();
+        camera = GetComponent<Camera>();
+    }
+
     private void Update()
     {
         CameraController();
+        if (player.IsMyTrun)
+        {
+            CameraInteraction();
+        }
     }
 
     void CameraController()
@@ -22,10 +36,18 @@ public class PlayerController : MonoBehaviour
         rotY -= Input.GetAxis("Mouse X") * sens;
         rotX += Input.GetAxis("Mouse Y") * sens;
         
-        rotX = Mathf.Clamp(rotX, -CameraClamp, CameraClamp);
+        rotX = Mathf.Clamp(rotX, -CameraClamp - CameraAngle, CameraClamp - CameraAngle);
         rotY = Mathf.Clamp(rotY, -CameraClamp, CameraClamp);
         
         Quaternion _target = Quaternion.Euler(-rotX, -rotY, 0);
         transform.rotation = Quaternion.Slerp(transform.rotation, _target, Time.deltaTime * Smooth);
+    }
+
+    void CameraInteraction()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        mousePos = camera.ScreenToWorldPoint(mousePos);
+        Debug.DrawRay(transform.position, mousePos * 100 - transform.position * 100, Color.red);
     }
 }
